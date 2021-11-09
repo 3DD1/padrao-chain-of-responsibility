2bf14cc897a6695d5a9320e8acaf44d5302660b4
Edmilson <scovinowork@gmail.com>
2021-11-09 00:03:15 -0300

    Classes tipo Software e Software

---
+++ src/main/java/Software.java
@@ -0,0 +1,14 @@
+public class Software {
+    private TipoSoftware tipoSoftware;
+    public Software(TipoSoftware tipoSoftware) {
+        this.tipoSoftware = tipoSoftware;
+    }
+
+    public TipoSoftware getTipoSoftware() {
+        return tipoSoftware;
+    }
+
+    public void setTipoSoftware(TipoSoftware tipoSoftware) {
+        this.tipoSoftware = tipoSoftware;
+    }
+}
