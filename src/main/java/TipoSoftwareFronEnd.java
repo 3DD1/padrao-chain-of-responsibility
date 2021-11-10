13fb1c4243e3973ab41c2e557e65cfff5dfab4ea
Edmilson <scovinowork@gmail.com>
2021-11-10 00:24:27 -0300

    up

--- src/main/java/TipoSoftwareFronEnd.java
+++ src/main/java/TipoSoftwareFronEnd.java
@@ -1,2 +1,11 @@
-public class TipoSoftwareFronEnd {
+public class TipoSoftwareFronEnd implements TypeSoftware{
+    private static TipoSoftwareFronEnd frontEnd = new TipoSoftwareFronEnd();
+
+    private TipoSoftwareFronEnd(){};
+
+    public static TipoSoftwareFronEnd geFrontEnd(){
+        return front-end;
+    }
+
+
 }
